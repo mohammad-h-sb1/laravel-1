68f34b5dbaab16d44d199beeae2d697a51a8cdca
mohammad_h_sb <mohammad.h.sb44@gmail.com>
2021-01-11 00:11:55 +0330

    add project

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $articles=DB::table('articles')->find(1);
+    dd($articles->title);
+    return view('index');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/contact', function () {
+    return view('Contact');
+});
+
+
+
+
+
